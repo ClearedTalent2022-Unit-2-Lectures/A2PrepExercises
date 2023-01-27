4a76a465ad23707b336c39a096b539cb01dc1742
pearce-odegard <pearceodegard@gmail.com>
2023-01-27 10:41:50 -0600

    Added nextId comment

--- src/org/launchcode/Person.java
+++ src/org/launchcode/Person.java
@@ -2,6 +2,13 @@ package org.launchcode;
 
 public abstract class Person {
     private int id;
+    // This nextId static property is great for giving each new Person
+    // its own unique id. Based on the code in the first constructor on line
+    // 12, when you create a new Person object, the id field will be set to whatever
+    // nextId currently is, which is 1 to start. Then, nextId iterates up by 1.
+    // Then when you create another Person object, it will get an id of 2, and nextId
+    // will again iterate up by 1. That way, the id of Person objects is now automatically
+    // being handled.
     private static int nextId = 1;
 
     private String firstName;
